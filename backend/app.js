fd3bc7525a17f3e3031ce416de89226a99b2051d
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-07 17:55:46 +0500

    fix: add error middleware and set query parser in app configuration

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+app.set("query parser", "extended");
+
+const errorMiddleware = require("./middleware/error.js");
 
 app.use(express.json());
 //Routes Import
 const product = require("./routes/productRoute.js");
 
 app.use("/api/v1", product);
+//Middleware for error
+app.use(errorMiddleware);
+
 module.exports = app;
