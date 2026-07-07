c1a34978838420864daed78d01cc83d4777e2dab
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-07 15:50:39 +0500

    fix: update product controller methods for async handling and response structure

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -9,6 +9,50 @@ exports.createProduct = async (req, res, next) => {
   });
 };
 // get All products
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route is working fine" });
+exports.getAllProducts = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  res.status(200).json({ success: true, products });
+};
+// get single  product details
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  res.status(200).json({ success: true, product });
+};
+//Update Product --Admin
+exports.updateProduct = async (req, res, next) => {
+  console.log(req.params.id);
+
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    returnDocument: "after",
+    runValidators: true,
+  });
+  res.status(200).json({ success: true, product });
+};
+// Delete Product
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  await Product.findByIdAndDelete(req.params.id);
+  res
+    .status(200)
+    .json({ success: true, message: "Product Delete Successfully" });
 };
