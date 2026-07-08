619e533eb286f8a1b3c9a548140ee2edcb1614a1
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-08 16:34:38 +0500

    feat: add user route integration in app.js

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,9 @@ const errorMiddleware = require("./middleware/error.js");
 app.use(express.json());
 //Routes Import
 const product = require("./routes/productRoute.js");
-
+const user = require("./routes/userRoute.js")
 app.use("/api/v1", product);
+app.use("/api/v1", user);
 //Middleware for error
 app.use(errorMiddleware);
 
