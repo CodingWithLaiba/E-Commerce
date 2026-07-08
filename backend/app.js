ccf2cd0c0ed263c71ddb470a9027fa22187f9c55
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-08 17:50:26 +0500

    feat: fix formatting and ensure cookie-parser middleware is applied

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 app.set("query parser", "extended");
 
 const errorMiddleware = require("./middleware/error.js");
 
 app.use(express.json());
+app.use(cookieParser());
 //Routes Import
 const product = require("./routes/productRoute.js");
-const user = require("./routes/userRoute.js")
+const user = require("./routes/userRoute.js");
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 //Middleware for error
