b0dc4f462b53f2b5b0d1c0015a1a88410df0e692
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-09 16:45:36 +0500

    feat: enhance error handling for JWT and duplicate key errors

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -8,6 +8,21 @@ module.exports = (err, req, res, next) => {
     const message = `Resource not found.Invalid: ${err.path}`;
     err = new ErrorHandler(message, 400);
   }
+  // mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+  // Wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `JSON Web Token is invalid, Try again`;
+    err = new ErrorHandler(message, 400);
+  }
+  // JWT Expire  error
+  if (err.name === "Token ExpiredError") {
+    const message = `JSON Web Token is Expired, Try again`;
+    err = new ErrorHandler(message, 400);
+  }
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
