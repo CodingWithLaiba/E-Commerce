a5799dfc69eefc5e050f1394844eadffd71469b9
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-09 16:45:45 +0500

    feat: add routes for password reset and forget password functionality

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,11 +3,16 @@ const {
   registerUser,
   loginUser,
   logout,
+  forgetPassword,
+  resetPassword,
 } = require("../controllers/userController");
 
 const router = express.Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/password/forget").post(forgetPassword);
+router.route("/password/reset/:token").put(resetPassword );
+
 router.route("/logout").get(logout);
 
 module.exports = router;
