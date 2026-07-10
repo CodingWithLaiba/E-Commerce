48062cda85f6f985f7ac07f60600ab13bcea9b71
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-10 15:19:21 +0500

    feat: enhance user routes with admin role management and user update functionalities

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,8 +7,13 @@ const {
   resetPassword,
   getUserDetails,
   updatePassword,
+  updateProfile,
+  getAllUsers,
+  getSingleUserDetailsByAdmin,
+  updateUserRole,
+  deleteUser,
 } = require("../controllers/userController");
-const { isAuthenticatedUser } = require("../middleware/auth");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 router.route("/register").post(registerUser);
@@ -19,5 +24,18 @@ router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
 router.route("/me").get(isAuthenticatedUser, getUserDetails);
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
+router
+  .route("/admin/user/:id")
+  .get(
+    isAuthenticatedUser,
+    authorizeRoles("admin"),
+    getSingleUserDetailsByAdmin,
+  )
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 module.exports = router;
