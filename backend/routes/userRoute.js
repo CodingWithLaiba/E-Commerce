d38b01126b741b2e727f97f09334ce425f955a90
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-10 14:36:08 +0500

    feat: add user details retrieval and password update routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,14 +5,19 @@ const {
   logout,
   forgetPassword,
   resetPassword,
+  getUserDetails,
+  updatePassword,
 } = require("../controllers/userController");
+const { isAuthenticatedUser } = require("../middleware/auth");
 
 const router = express.Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/password/forget").post(forgetPassword);
-router.route("/password/reset/:token").put(resetPassword );
+router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/logout").get(logout);
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 
 module.exports = router;
