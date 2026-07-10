fd39e978ac0a95f578b20e06480e862d8d6a0301
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-10 16:22:34 +0500

    feat: add review routes for creating, retrieving, and deleting product reviews

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,6 +5,9 @@ const {
   updateProduct,
   deleteProduct,
   getProductDetails,
+  createProductReview,
+  deleteReview,
+  getProductReviews,
 } = require("../controllers/productController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
@@ -18,5 +21,9 @@ router
   .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 router.route("/product/:id").get(getProductDetails);
-
+ router.route("/review").put(isAuthenticatedUser,createProductReview);
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 module.exports = router;
